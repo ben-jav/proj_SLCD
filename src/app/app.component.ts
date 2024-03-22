6ae5fb8f3f35c21f38920cb9105e6c241dd7d9c2
ben-jav <a.javadi@web.de>
2024-03-22 12:02:28 +0100

    Methoden vereinfachen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import AFRIKA from '../app/data/Afrika.json';
 import ASIA from '../app/data/Asia.json';
 import EUROPA from '../app/data/Europa.json';
-import SUDAMERIKA from '../app/data/Suedamerika.json';
+import SUDAMERIKA from '../app/data/Sudamerika.json';
 import { Tier } from './tierInterface';
 import { TiereContinent } from './tierInterface';
 
@@ -17,10 +17,13 @@ export class AppComponent {
   europaData: TiereContinent = EUROPA;
   sudamerikaData: TiereContinent = SUDAMERIKA;
 
-  newTierName: string = '';
+  selectedContinentData: TiereContinent = AFRIKA;
+  continentName: string = '';
 
+  newTierName: string = '';
+  
   // constructor() {
-  //   this.loadData();
+    // this.loadData();
   // }
 
   // loadData() {
@@ -30,55 +33,65 @@ export class AppComponent {
   //   this.sudamerikaData = SUDAMERIKA;
   // }
 
-  addNewTier(continent: string) {
-    let data: TiereContinent;
-
-    switch(continent) {
+  selectContinent() {
+    let selectContinentName = this.continentName;
+    switch(selectContinentName) {
       case 'Afrika':
-        data = this.afrikaData;
+        this.selectedContinentData = this.afrikaData;
         break;
       case 'Asia':
-        data = this.asiaData;
+        this.selectedContinentData = this.asiaData;
         break;
       case 'Europa':
-        data = this.europaData;
+        this.selectedContinentData = this.europaData;
         break;
       case 'Sudamerika':
-        data = this.sudamerikaData;
+        this.selectedContinentData = this.sudamerikaData;
         break;
       default:
         console.log('Falsche Eingabe!');
         return;
-    }
-    if (this.newTierName) {
-      const newTier: Tier = { name: this.newTierName };
-      data.tiere.push(newTier);
-      this.newTierName = '';
     }
   }
 
-  downloadData(continent: string) {
+  addNewTier(continent: string) {
     let data: TiereContinent;
-
-    switch(continent) {
-      case 'Afrika':
-        data = this.afrikaData;
-        break;
-      case 'Asia':
-        data = this.asiaData;
-        break;
-      case 'Europa':
-        data = this.europaData;
-        break;
-      case 'Sudamerika':
-        data = this.sudamerikaData;
-        break;
-      default:
-        console.log('Falsche Eingabe!');
-        return;
+    if (this.newTierName) {
+      const newTier: Tier = { name: this.newTierName };
+      this.selectedContinentData.tiere.push(newTier);
+      this.newTierName = '';
     }
+  }
+  // addNewTier(continent: string) {
+  //   let data: TiereContinent;
+
+  //   switch(continent) {
+  //     case 'Afrika':
+  //       data = this.afrikaData;
+  //       break;
+  //     case 'Asia':
+  //       data = this.asiaData;
+  //       break;
+  //     case 'Europa':
+  //       data = this.europaData;
+  //       break;
+  //     case 'Sudamerika':
+  //       data = this.sudamerikaData;
+  //       break;
+  //     default:
+  //       console.log('Falsche Eingabe!');
+  //       return;
+  //   }
+  //   if (this.newTierName) {
+  //     const newTier: Tier = { name: this.newTierName };
+  //     data.tiere.push(newTier);
+  //     this.newTierName = '';
+  //   }
+  // }
+
+  downloadData(continent: string) {
     const fileName = `${continent}_data.json`
-    const fileContent = JSON.stringify(data, null, 2);
+    const fileContent = JSON.stringify(this.selectedContinentData, null, 2);
     const blob = new Blob([fileContent], {type: 'application/json'});
     const url = window.URL.createObjectURL(blob);
 
@@ -90,26 +103,57 @@ export class AppComponent {
     window.URL.revokeObjectURL(url);
   }
 
+  // downloadData(continent: string) {
+  //   let data: TiereContinent;
 
-  generateContinentSection(continentData: TiereContinent, continentName: string): string {
-    if (!continentData) {
-      return '';
-    }
-    return `
-      <div>
-        <h2>${continentName} Daten</h2>
-        <ul>
-          <li *ngFor="let tier of ${continentName.toLowerCase()}Data.tiere">{{tier.name}}</li>
-        </ul>
-        <form (submit)="addNewTier('${continentName}')">
-          Neues Tier: <input [(ngModel)]="newTierName" name="newTierName">
-          <button type="submit">Hinzufügen</button>
-          <button (click)="downloadData('${continentName}')">Daten herunterladen</button>
-        </form>
-      </div>
-      <hr>
-    `;
-  }
-  
+  //   switch(continent) {
+  //     case 'Afrika':
+  //       data = this.afrikaData;
+  //       break;
+  //     case 'Asia':
+  //       data = this.asiaData;
+  //       break;
+  //     case 'Europa':
+  //       data = this.europaData;
+  //       break;
+  //     case 'Sudamerika':
+  //       data = this.sudamerikaData;
+  //       break;
+  //     default:
+  //       console.log('Falsche Eingabe!');
+  //       return;
+  //   }
+  //   const fileName = `${continent}_data.json`
+  //   const fileContent = JSON.stringify(data, null, 2);
+  //   const blob = new Blob([fileContent], {type: 'application/json'});
+  //   const url = window.URL.createObjectURL(blob);
 
+  //   const link = document.createElement('a');
+  //   link.href = url;
+  //   link.download = fileName;
+  //   link.click();
+
+  //   window.URL.revokeObjectURL(url);
+  // }
+
+
+  // generateContinentSection(continentData: TiereContinent, continentName: string): string {
+  //   if (!continentData) {
+  //     return '';
+  //   }
+  //   return `
+  //     <div>
+  //       <h2>${continentName} Daten</h2>
+  //       <ul>
+  //         <li *ngFor="let tier of ${continentName.toLowerCase()}Data.tiere">{{tier.name}}</li>
+  //       </ul>
+  //       <form (submit)="addNewTier('${continentName}')">
+  //         Neues Tier: <input [(ngModel)]="newTierName" name="newTierName">
+  //         <button type="submit">Hinzufügen</button>
+  //         <button (click)="downloadData('${continentName}')">Daten herunterladen</button>
+  //       </form>
+  //     </div>
+  //     <hr>
+  //   `;
+  // }
 }
